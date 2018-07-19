5762b760cb1bf132a004df7c0e100efd574683e1
MoonSamShang <moon@occ.co.jp>
2018-07-19 16:33:28 +0900

    First source code

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string greeting = "Hello";
+			string name = "NMSS";
+			Console.WriteLine($"{greeting} my name is {name}.");
+			Console.ReadLine();
+		}
+	}
+}
